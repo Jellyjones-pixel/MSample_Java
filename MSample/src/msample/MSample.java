b5460cacbe72cf8e702acf0da28bf2c194521d84
JOHN MBITHI MUTAVE <83606270+Jellyjones-pixel@users.noreply.github.com>
2023-09-27 16:50:36 +0100

    Add files via upload
    Updated

---
+++ MSample/src/msample/MSample.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package msample;
+
+/**
+ *
+ * @author jelly_joneske
+ */
+public class MSample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int arr[][]={{1,2,3},{2,3,4},{4,4,5}};
+        for (int i=0; i<3; i++){
+        for (int j=0; j<3; j++){
+            System.out.println(arr[i] [j]+ "");
+        }
+        System.out.print("arr");
+    }
+    }
+    
+}
